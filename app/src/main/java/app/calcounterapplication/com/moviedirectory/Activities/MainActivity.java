a40b36208b6a487f697643d27726da8bde8b9822
Khusboo <khusboo1299@gmail.com>
2020-09-21 23:39:32 +0530

    Movie Directory

--- app/src/main/java/app/calcounterapplication/com/moviedirectory/Activities/MainActivity.java
+++ app/src/main/java/app/calcounterapplication/com/moviedirectory/Activities/MainActivity.java
@@ -1,23 +1,48 @@
 package app.calcounterapplication.com.moviedirectory.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.calcounterapplication.com.moviedirectory.Model.Movie;
 import app.calcounterapplication.com.moviedirectory.R;
+import app.calcounterapplication.com.moviedirectory.Util.Constants;
+import app.calcounterapplication.com.moviedirectory.Util.Prefs;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter movieRecyclerViewAdapter;
+    private List<Movie> movieList;
+    private RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +50,61 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        queue = Volley.newRequestQueue(this);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movieList = new ArrayList<>();
+        Prefs prefs = new Prefs(MainActivity.this);
+        String search = prefs.getSearch();
+        getMovies(search);
+    }
+
+    //Get movies
+    public List<Movie> getMovies(String searchItem) {
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL_LEFT + searchItem + Constants.URL_RIGHT, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray moviesArray = response.getJSONArray("Search");
+
+                    for (int i=0; i<moviesArray.length(); i++){
+                        JSONObject movieObj = moviesArray.getJSONObject(i);
+                        Movie movie = new Movie();
+                        movie.setTitle(movieObj.getString("Title"));
+                        movie.setYear(movieObj.getString("Year"));
+                        movie.setMovieType(movieObj.getString("Type"));
+                        movie.setPoster(movieObj.getString("Poster"));
+                        movie.setImdbId(movieObj.getString("imdbId"));
+
+                        Log.d("Movies", movie.getTitle());
+                    }
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(jsonObjectRequest);
+        return movieList;
     }
 
     @Override
---
+++ app/src/main/java/app/calcounterapplication/com/moviedirectory/Util/Constants.java
@@ -0,0 +1,6 @@
+package app.calcounterapplication.com.moviedirectory.Util;
+
+public class Constants {
+    public static final String URL_LEFT = "http://www.omdbapi.com/";
+    public static final String URL_RIGHT = "&page=2";
+}
